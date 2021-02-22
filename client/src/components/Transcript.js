393fd2add947f65e6061deea4adafc30434d7c1f
Junyoung <kensiro5218@gmail.com>
2021-02-22 19:03:36 +0800

    updated stt and websocket connectivity

--- client/src/components/Transcript.js
+++ client/src/components/Transcript.js
@@ -1,40 +1,92 @@
 import React, { useEffect } from 'react';
 import { SpeechToText } from 'watson-speech';
+import { io } from 'socket.io-client';
 
 const userMedia = async () => {
-  const micMedia = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
+  /* On Windows and Chrome OS the entire system audio can be captured, 
+     but on Linux and Mac only the audio of a tab can be captured. */
+    Promise.all([
+      navigator.mediaDevices.getUserMedia({ video: false, audio: true}),
+      navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+    ])
+    .then(streams => {
+      const [micMedia, displayMedia] = streams;
+      if(displayMedia.getAudioTracks().length < 1) {
+        throw 'display audio share permission is denied';
+      }
 
-  //On Windows and Chrome OS the entire system audio can be captured, 
-  //but on Linux and Mac only the audio of a tab can be captured.
-  const desktopMedia = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true})
+      const mediaStreams = {
+        video: new MediaStream(displayMedia.getVideoTracks()),
+        sAudio: new MediaStream(displayMedia.getAudioTracks()),
+        mAudio: new MediaStream(micMedia.getAudioTracks())
+      };
 
-  const videoStream = new MediaStream(desktopMedia.getVideoTracks());
-  const audioStreams = {
-    sAudio: new MediaStream(desktopMedia.getAudioTracks()),
-    mAudio: new MediaStream(micMedia.getAudioTracks())
-  };
+      listenSTT(mediaStreams)
+        .then(() => recordMedia(mediaStreams));
+      
+    })
+    .catch(error => console.error(error));
 
-  listenSTT(audioStreams);
 };
 
-const listenSTT = (audioStreams) => {
-  fetch('http://localhost:5000/')
+const listenSTT = (mediaStreams) => {
+  return fetch('http://localhost:5000/api/stt/token/')
     .then(res => res.json())
-    .then(token => {
-      //refer to the below repo
-      //https://github.com/watson-developer-cloud/speech-javascript-sdk/blob/master/speech-to-text/recognize-stream.js
+    .then(({ accessToken, url }) => {
+      for (const [type, stream] of Object.entries(mediaStreams)) {
+        if (type !== 'video') { 
+          //refer to the below repo
+          //https://github.com/watson-developer-cloud/speech-javascript-sdk/blob/master/docs/SPEECH-TO-TEXT.md          
+          const sttStream = SpeechToText.recognizeMicrophone({
+            accessToken: accessToken,
+            url: url,
+            mediaStream: stream,
+            model: 'ko-KR_BroadbandModel', //ko-KR_BroadbandModel, ko-KR_NarrowbandModel
+            readableObjectMode: true,
+            objectMode: true,
+            inactivityTimeout: -1
+          });
 
+          sttStream.recognizeStream.on('open', () => {
+            console.info(`stt service ${type} connected`)
+          });
+
+          sttStream.on('data', data => {
+            const result = data.results[0];
+            if (result.final) {
+              const transcript = {...result.alternatives[0], type: type}
+              console.log(transcript)
+            }
+          })
+
+          sttStream.on('error', err => {
+            throw console.error(err)
+          });
+        }
+      }
+     
     });
 }
 
+const recordMedia = (mediaStreams) => {
+  const socket = io('http://localhost:5000/');
 
-const recordMedia = () => {
-  const mediaStream = new MediaStream(/* media stream */);
-  const rec = new MediaRecorder(mediaStream);
-  rec.ondataavailable = (e) => {
-    console.log(e.data)
-  };
-  rec.start(1000);
+  socket.on('connect', () => {
+    console.info('socket connection established ')
+  });
+  
+  for(const [type, stream] of Object.entries(mediaStreams)) {
+    const options = type === 'video' ? 
+      { mimeType: 'video/webm;codecs=H264', videoBitsPerSecond: 25000 } : 
+      { mimeType: 'audio/webm', audioBitsPerSecond: 6400 };
+
+    const recorder = new MediaRecorder(stream, options);
+    recorder.ondataavailable = (e) => {
+      socket.emit('data', {[type]: e.data});
+    };
+
+    recorder.start(1000);
+  }
 }
   
 
--- server/server.js
+++ server/server.js
@@ -3,21 +3,53 @@ const app = express();
 const http = require('http');
 const cors = require('cors');
 const config = require('./config.json');
+const { IamTokenManager } = require('ibm-watson/auth');
 
 const PORT = config.port;
 
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(cors());
 
 io.on('connection', socket => {
-    console.log('socket connected');
+    console.info(`socket-${socket.id}  connected`);
+    const video = sAudio = mAudio = [];
+    socket['buffers'] = {video, sAudio, mAudio};
+
+    socket.on('data', data => {
+        const [type, buffer] = Object.entries(data)[0];
+        socket['buffers'][type].push(buffer);
+    });
+
+    socket.on('disconnect', () => {
+        for (const bufferArray of Object.values(socket['buffers'])) {
+            const buffer = Buffer.concat(bufferArray);
+
+            //save buffer into DB
+        }
+
+        socket.disconnect();
+    })
+});
+
+const sttAuthenticator = new IamTokenManager({
+    apikey: config.sttApiKey
 });
 
 //refer to https://github.com/watson-developer-cloud/speech-javascript-sdk/tree/master/examples/
-app.get('/', (req, res) => {
-    res.json('hello world');
+app.get('/api/stt/token', (req, res) => {
+    return sttAuthenticator
+    .requestToken()
+    .then(({ result }) => {
+      res.json({ accessToken: result.access_token, url: config.sttUrl });
+    })
+    .catch(console.error);
 });
 
 server.listen(PORT, () => {
